2aa2b96243cb0ec059fd887c0712edf572ad9356
infodusha <infodusha@ya.ru>
2023-07-12 10:37:44 +0300

    fix: update

--- content.js
+++ content.js
@@ -1,19 +1,19 @@
 const key = 'authuser';
 const manifestEl = document.querySelector('link[rel=manifest]');
+const currentURL = new URL(window.location.href);
+const authuser = currentURL.searchParams.get(key);
 
-if (manifestEl) {
+if (manifestEl && authuser) {
     const url = new URL(manifestEl.href);
-    if (url.searchParams.has(key)) {
-        fetch(url)
-            .then(response => response.json())
-            .then((manifest) => modify(manifest, url))
-            .catch(console.error);
-    }
+    fetch(url)
+        .then(response => response.json())
+        .then((manifest) => modify(manifest, url))
+        .catch(console.error);
 }
 
 function modify(manifest, url) {
     const startUrl = new URL(manifest.start_url, url);
-    startUrl.searchParams.set(key, url.searchParams.get(key))
+    startUrl.searchParams.set(key, authuser)
     const newManifest = { ...manifest, start_url: startUrl.toString() };
     const blob = new Blob([JSON.stringify(newManifest)], { type: 'application/json' });
     const manifestURL = URL.createObjectURL(blob);
